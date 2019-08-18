72a2580988b0ec71ae2a3bfd3b314918b639df15
Massimo Zambelli <mass.zambelli@gmail.com>
2019-08-18 14:49:05 +0200

    folder organisation

--- snake-react/src/containers/App/index.tsx
+++ snake-react/src/containers/App/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.scss";
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,6 +19,4 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
-
-export default App;
+};
